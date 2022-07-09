9d9e31ae78d7c60c873374bb02be26edaaaf2202
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2022-07-09 18:27:10 -0500

    Metodo de editar version 1

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, json
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/tienda.db'
@@ -11,32 +14,42 @@ class Producto(db.Model):
     nombre = db.Column(db.String(250))
     precio = db.Column(db.Integer())
 
+class ProductoForm(FlaskForm):
+    codigo_barra = StringField('codigo_barra', validators=[DataRequired()])
+    nombre       = StringField('nombre', validators=[DataRequired()])
+    precio       = IntegerField('precio', validators=[DataRequired()])    
+
 @app.route("/")
 def main():    
     return render_template("index.html")
 
 @app.route("/productos")
 def listar_productos():        
-    productos = db.session.execute('SELECT * FROM producto').fetchall()
+    productos = db.session.execute("SELECT * FROM producto").fetchall()
     db.session.close()
     return render_template("productos.html", productos = productos)
 
-@app.route("/crear-producto", methods=["POST"])
+@app.route("/crear_producto", methods=["POST"])
 def crear_producto():
     agregar_produco = Producto(codigo_barra = request.form["codigo_barra"], nombre = request.form["nombre"], precio = request.form["precio"])
     db.session.add(agregar_produco)
     db.session.commit()
+    db.session.close()
     return redirect(url_for("listar_productos"))
 
-@app.route("/editar/<id>")
-def editar_producto(id):
-    producto = Producto.query.get(id)
-    return redirect(url_for("listar_productos"))
+@app.route("/editar", methods=["POST"])
+def editar():
+    producto = db.session.execute("SELECT * FROM producto WHERE id = :id", {"id": request.form.get("id")}).fetchall()
+    result = []
+    result.append({'id':producto[0][0],'codigo_barra':producto[0][1],'nombre':producto[0][2],'precio':producto[0][3]})
+    db.session.close()
+    return json.dumps(result)
 
-@app.route("/eliminar/<id>")
+@app.route("/eliminar_producto/<id>")
 def eliminar_producto(id):
     Producto.query.filter_by(id=int(id)).delete()
     db.session.commit()
+    db.session.close()
     return redirect(url_for("listar_productos"))
 
 if __name__ == "__main__":
